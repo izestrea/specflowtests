2ad3c5b841ae24a76a9c34543053ed4ce8ac871b
izestrea <izestrea@gmail.com>
2016-08-04 16:00:07 +0300

    Adding scenario outline step definitions

---
+++ SpecFlowTests/TeethWhitenessSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests
+{
+    [Binding]
+    public class TeethWhitenessSteps
+    {
+        [Given(@"I'm using ""(.*)"" brand toothpaste")]
+        public void GivenIMUsingBrandToothpaste(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I brush for (.*) minutes")]
+        public void WhenIBrushForMinutes(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the teeth look (.*) white")]
+        public void ThenTheTeethLookWhite(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
